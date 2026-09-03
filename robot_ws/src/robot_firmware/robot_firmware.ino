a0908e7e3864dd26487b1fe5de86c9ce3397cfcd
Lucas <lucaszhang1118@gmail.com>
2026-09-03 09:56:00 -0700

    Add encoder ISR scaffolding to firmware.

--- robot_ws/src/robot_firmware/robot_firmware.ino
+++ robot_ws/src/robot_firmware/robot_firmware.ino
@@ -68,3 +68,73 @@ void rightEncoderISR() {
 static int clampPwm(int value) {
   if (value > 255) {
     return 255;
+  }
+  if (value < -255) {
+    return -255;
+  }
+  return value;
+}
+
+void setSidePwm(uint8_t pin_en, uint8_t pin_rpwm, uint8_t pin_lpwm, int pwm) {
+  pwm = clampPwm(pwm);
+  if (pwm == 0) {
+    digitalWrite(pin_en, LOW);
+    analogWrite(pin_rpwm, 0);
+    analogWrite(pin_lpwm, 0);
+    return;
+  }
+
+  digitalWrite(pin_en, HIGH);
+  if (pwm > 0) {
+    analogWrite(pin_rpwm, pwm);
+    analogWrite(pin_lpwm, 0);
+  } else {
+    analogWrite(pin_rpwm, 0);
+    analogWrite(pin_lpwm, -pwm);
+  }
+}
+
+void applyMotorCommand(int left_pwm, int right_pwm) {
+  setSidePwm(PIN_LEFT_EN, PIN_LEFT_RPWM, PIN_LEFT_LPWM, left_pwm);
+  setSidePwm(PIN_RIGHT_EN, PIN_RIGHT_RPWM, PIN_RIGHT_LPWM, right_pwm);
+}
+
+void stopMotors() {
+  g_cmd_left = 0;
+  g_cmd_right = 0;
+  applyMotorCommand(0, 0);
+}
+
+bool parseMotorCommand(const char *line) {
+  int left_pwm = 0;
+  int right_pwm = 0;
+
+  
+  if (sscanf(line, "L%d R%d", &left_pwm, &right_pwm) != 2) {
+    if (sscanf(line, "L %d R %d", &left_pwm, &right_pwm) != 2) {
+      return false;
+    }
+  }
+
+  g_cmd_left = clampPwm(left_pwm);
+  g_cmd_right = clampPwm(right_pwm);
+  g_last_cmd_ms = millis();
+  applyMotorCommand(g_cmd_left, g_cmd_right);
+  return true;
+}
+
+void pollSerial() {
+  while (Serial.available() > 0) {
+    char c = (char)Serial.read();
+    if (c == '\r') {
+      continue;
+    }
+    if (c == '\n') {
+      g_serial_buf[g_serial_len] = '\0';
+      if (g_serial_len > 0) {
+        parseMotorCommand(g_serial_buf);
+      }
+      g_serial_len = 0;
+      continue;
+    }
+    if (g_serial_len + 1 < SERIAL_BUF_LEN) {
